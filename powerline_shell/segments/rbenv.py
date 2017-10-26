60765710524c8191bf6eb0bd8afd8f6344f60336
Arthur Barton (arthurb) <arthurb@igniferous.net>
2017-10-26 11:39:57 +1100

    merge from upstream + include local config

---
+++ powerline_shell/segments/rbenv.py
@@ -0,0 +1,20 @@
+import subprocess
+from ..utils import BasicSegment
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        powerline = self.powerline
+        try:
+            p1 = subprocess.Popen(["rbenv", "version-name"], stdout=subprocess.PIPE)
+            version = p1.communicate()[0].decode("utf-8").rstrip()
+            if len(version) <= 0:
+                    return
+            elif version == "system":
+                    return
+            else:
+                powerline.append(' %s ' % version,
+                        powerline.theme.VIRTUAL_ENV_FG,
+                        powerline.theme.VIRTUAL_ENV_BG)
+        except OSError:
+            return
