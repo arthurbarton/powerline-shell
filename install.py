33083e64a94dfd7c967cfa2ee7bad182aa3539ea
Arthur Barton (arthurb) <arthurb@igniferous.net>
2017-10-26 12:37:56 +1100

    not needed

--- install.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-subprocess.run(["cp", "my-config.json", "~/.powerline-shell.json"], check=True)
-subprocess.run(["python3", "setup.py", "build"], check=True)
-subprocess.run(["python3", "setup.py", "install", "--root=$HOME/.local"], check=True)
