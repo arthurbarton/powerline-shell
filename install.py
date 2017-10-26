ed3414d034ae91b5ab336bc34775ff90a35d21cf
Arthur Barton (arthurb) <arthurb@igniferous.net>
2017-10-26 12:13:19 +1100

    copy the config also

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 import subprocess
+subprocess.run(["cp", "my-config.json", "~/.powerline-shell.json"], check=True)
 subprocess.run(["python3", "setup.py", "build"], check=True)
 subprocess.run(["python3", "setup.py", "install", "--root=$HOME/.local"], check=True)
