8ce1d058440d35e8485500326219f1b3b7dbcc3d
Arthur Barton (arthurb) <arthurb@igniferous.net>
2017-10-26 11:58:54 +1100

    install locally

--- install.py
+++ install.py
@@ -2,4 +2,4 @@
 
 import subprocess
 subprocess.run(["python3", "setup.py", "build"], check=True)
-subprocess.run(["python3", "setup.py", "install"], check=True)
+subprocess.run(["python3", "setup.py", "install", "--root=$HOME/.local"], check=True)
